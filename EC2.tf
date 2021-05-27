03f31130fe9b5b7718c06192e44274925b9c965a
Michael M <79147718+mmah001@users.noreply.github.com>
2021-05-28 00:47:16 +0800

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,11 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-}
+
 
 resource "aws_instance" "example" {
   ami           = "ami-0cf6f5c8a62fa5da6"
