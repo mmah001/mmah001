b1a5d4c83da88efc3f92d0f2bd7ca5d1d057ccd3
Michael M <79147718+mmah001@users.noreply.github.com>
2021-05-28 00:47:04 +0800

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -7,13 +7,6 @@ terraform {
   }
 }
 
-provider "aws" {
-  profile = "default"
-  access_key = "AKIAQBB3LYJECAR6UBZN"
-  secret_key = "Ke+EKG+2HEK9t+dN7liUFdEQS5C8cWm9NV6lA9by"
-  region  = "us-west-2"
-}
-
 resource "aws_instance" "example" {
   ami           = "ami-0cf6f5c8a62fa5da6"
   instance_type = "t2.micro"
